a4bfacf9d9c234d685c486666f1e065473cdb694
坏黑 <sky@asgard.me>
2018-03-06 20:01:22 +0800

    更新：主类代码

--- src/main/java/src/com/github/bkm016/bsteams/BSTeamsPlugin.java
+++ src/main/java/src/com/github/bkm016/bsteams/BSTeamsPlugin.java
@@ -1,9 +1,33 @@
 package com.github.bkm016.bsteams;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import lombok.Getter;
+
 /**
  * @author sky
  * @since 2018-03-06 19:41:07
  */
-public class BSTeamsPlugin {
-
+public class BSTeamsPlugin extends JavaPlugin {
+	
+	@Getter
+	private static Plugin inst;
+	
+	@Override
+	public void onLoad() {
+		inst = this;
+		// ±£´æÅäÖÃÎÄ¼ş
+		saveDefaultConfig();
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
 }
